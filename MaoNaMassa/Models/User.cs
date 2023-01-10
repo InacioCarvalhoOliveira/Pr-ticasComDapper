e126925ff870738bcdd4738ede819c6db5122187
InacioCarvalhoOliveira <inacio.cavalho.oliveira02@gmail.com>
2023-01-09 23:18:15 -0300

    new row

--- MaoNaMassa/Models/User.cs
+++ MaoNaMassa/Models/User.cs
@@ -1,13 +1,18 @@
+
+using Dapper.Contrib.Extensions;
+
 namespace PraticasComDapper.MaoNaMassa.Models
 {
+    [Table("[User]")]
     public class User
     {
-        public int Guid { get; set; }
-        //public string Name { get; set; }
-        //public string PasswordHash { get; set; }
-        //public string Bio { get; set; }
-        //public string Image { get; set; }
-        //public string Slug { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        public string Slug { get; set; }
     }
     
 }
